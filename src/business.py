c225dea0b6573830d77a8782919c0ab505b7d38c
George Danezis <gdanezis@fb.com>
2020-03-02 17:49:08 +0000

    [protocol] WIP on how to link high-level commands to lower level protocol
    * Simplified the structure of the SharedObject (may fold into the StructureChecker) in the future.
    * Defined a ProtocolExecutor to execute (speculativelly) sequences of commands.
    * Defined skeleton ProtocolCommand and specialized PaymentCommand. May fold into
      CommandRequestObject in the future
    * Tests for an example run, with commands failing if objects are used.

--- src/business.py
+++ src/business.py
@@ -1,164 +1,9 @@
 """ Define the 'business logic' for the Off-chain protocols """
 
-""" The Payment object status is defined by the status of both actors, senders and
-    receivers, namely the tuple (sender_status, recipient_status). An actor status may
-    have the following values:
-
-
-V0 States
----------
-
-        * None                      -- denotes the status of
-                                       an object that does not exist
-        * needs_stable_id           -- requests the other VASP for
-                                       a stable identifier for
-                                       the payment recipient.
-        * needs_kyc_data            -- requires the other VASP
-                                       to provide KYC data.
-        * ready_for_settlement      -- signals that the party is ready to settle
-                                       the transaction.
-        * needs_recipient_signature -- requests the recipient VASP to sign the
-                                       identifier for this transaction to put
-                                       it on chain.
-        * signed                    -- The recipient signed the transaction to
-                                       settle
-        * settled                   -- a Libra transaction settles this
-                                       transaction
-        * abort                     -- signals that the transactions is to be
-                                       aborted.
-
-V1 States
----------
-
-        * in_batch                  -- payment is included in a batch
-
-The allowable state transitions are as follows:
-
--- CREATE OBJECTS & Inquire for stable ID
-
-Sender Actor
-------------
-(None, None) -> (needs_stable_id, None)
-             -> (ready_for_settlement, None)
-             -> (needs_recipient_signature, None)
-
-Recipient Actor
----------------
-(needs_stable_id, None) -> (needs_stable_id, needs_stable_id)
-                        -> (needs_stable_id, needs_kyc_data)
-                        -> (needs_stable_id, ready_for_settlement)
-
-(ready_for_settlement, None) -> (ready_for_settlement, ready_for_settlement)
-
-Sender
-------
-(needs_stable_id, needs_stable_id)      -> (ready_for_settlement, needs_stable_id)
-                                        -> (needs_kyc_data, needs_stable_id)
-(needs_stable_id, needs_kyc_data)       -> (needs_kyc_data, needs_kyc_data)
-                                        -> (ready_for_settlement, needs_kyc_data)
-(needs_stable_id, ready_for_settlement) -> (ready_for_settlement, ready_for_settlement)
-
-Recipient
----------
-(ready_for_settlement, needs_stable_id) -> (ready_for_settlement, ready_for_settlement)
-
--- EXCHANGE KYC FLOWS
-
-Recipient
----------
-(needs_kyc_data, needs_stable_id) -> (needs_kyc_data, needs_kyc_data)
-                                  -> (needs_kyc_data, ready_for_settlement)
-
-Sender or Recipient
--------------------
-(needs_kyc_data, needs_kyc_data) -> (needs_kyc_data, needs_kyc_data)
-
-Sender
-------
-(needs_kyc_data, needs_kyc_data) -> (ready_for_settlement, needs_kyc_data)
-(needs_kyc_data, ready_for_settlement) -> (ready_for_settlement, ready_for_settlement)
-
-Recipient
----------
-(needs_kyc_data, needs_kyc_data)       -> (needs_kyc_data, ready_for_settlement)
-(ready_for_settlement, needs_kyc_data) -> (ready_for_settlement, ready_for_settlement)
-
--- ON CHAIN SETTLEMENT FLOWS -- ONE PAYMENT
-
-Sender
-------
-(ready_for_settlement, ready_for_settlement) -> (needs_recipient_signature, ready_for_settlement)
-
-Recipient
----------
-(needs_recipient_signature, None) -> (needs_recipient_signature, signed)
-(needs_recipient_signature, ready_for_settlement) -> (needs_recipient_signature, signed)
-
-Sender
-------
-(needs_recipient_signature, signed) -> (settled, signed)
-
-Recipient
----------
-(settled, signed) -> (settled, settled)
-
-#-- PAYMENT BATCH FLOWS (v1)
-#
-#Sender or Recipient
-#-------------------
-#(ready_for_settlement, ready_for_settlement) -> in_batch
-#in_batch -> (ready_for_settlement, ready_for_settlement)
-#
-#in_batch -> (settled, settled)
-
--- ABORT FLOWS
-
-* = all states except { settled }
-
-Sender
-------
-(*, *) -> (Abort, *)
-(*, Abort) -> (Abort, Abort)
-
-Recipient
----------
-(*, *) -> (*, Abort)
-(Abort, *) -> (Abort, Abort)
-<<<<<<< HEAD
-=======
-
---------------------------------------------------
-
-VASP sub-commands to implement all transitions:
-
-.create_payment()
-    Creates a new payment with all mandatory fields set.
-
-.set_signature()
-    Used by receiver to sign the Payment reference ID.
-
-.add_<sender|recipient>_stable_id()
-    Used to include the stabe ID for the user at either VASPs.
-
-.add_<sender|recipient>_extended_kyc()
-    Used to include the extended KYC information at either VASP.
-
-.change_<sender|recipient>_status()
-    Update the status of the payment at either VASP.
-
-.add_<sender|recipient>_metadata()
-    Change the metadata field at either VASP.
-
->>>>>>> [protocol] Porting off-chain prototype files to github
-"""
-
 # ---------------------------------------------------------------------------
 
 ''' These interfaces are heavily WIP, as we decide how to best implement the
     above state machines '''
-
-
-
 
 from os import urandom
 from base64 import standard_b64encode
@@ -168,79 +13,70 @@ def get_unique_string():
 
 # Generic interface to a shared object
 
-
 class SharedObject:
     def __init__(self):
         ''' All objects have a version number and their commit status '''
         self.version = get_unique_string()
-        self.decided = None
+        self.extends = [] # Strores the version of the previous object
 
     def new_version(self):
         ''' Make a deep copy of an object with a new version number '''
         clone = deepcopy(self)
+        clone.extends = [ self.get_version() ]
         clone.version = get_unique_string()
-        clone.decided = None
         return clone
 
     def get_version(self):
         ''' Return a unique version number to this object and version '''
         return self.version
 
-    def is_decided(self):
-        ''' Denotes the commands leading to this object have been sequenced.
-            They could be successes or failures.'''
-        return self.decided is not None
-
-    def is_success(self):
-        ''' Denotes whether the object has been committed to the sequence'''
-        return self.is_decided() and self.decided
-
-    def commit_success(self):
-        ''' Tag this object as a success if all requests leading to it were
-            a success '''
-        self.decided = True
-
-    def commit_failure(self):
-        ''' Tag this object as a failure if any command leading to it is a
-            failure'''
-        self.decided = False
-
 
 # Interface we need to do commands:
 class ProtocolCommand:
-
     def __init__(self, command):
+        self.depend_on = []
+        self.creates   = []
+        self.command   = command
+
+class ProtocolExecutor:
+    def __init__(self):
+        self.potentially_live = set()
+        self.actually_live    = set()
+        self.seq = []
+
+    def sequence_next_command(self, command):
+        if set(command.depend_on).issubset(self.potentially_live):
+            pos = len(self.seq)
+            self.seq += [ command ]
+            self.potentially_live |= set(command.creates)
+            return pos
+        else:
+            # We cannot sequence this, no way.
+            raise Exception()
+
+    def own_success(self, seq_no):
+        command = self.seq[seq_no]
+        # Consumes old objects
+        self.actually_live -= set(command.depend_on)
+        self.potentially_live -= set(command.depend_on)
+        # Creates new objects
+        self.actually_live |= set(command.creates)
         pass
 
-    def dependent_objects(self):
-        ''' Returns a list of object-IDs that need to exist for this command
-            to have a chance to succeed '''
+    def own_fail(self, seq_no):
+        command = self.seq[seq_no]
+        self.potentially_live -= set(command.creates)
         pass
 
-    def creates_objects(self):
-        ''' Lists the object-IDs created if this command succeeds '''
-        pass
+    def other_get_success(self, seq_no):
+        command = self.seq[seq_no]
+        if set(command.depend_on).issubset(self.actually_live):
+            self.own_success(seq_no)
+            return True
+        else:
+            self.own_fail(seq_no)
+            return False
 
-    def try_command(self):
-        ''' Called to check the command is acceptable, and return a command
-            success or failure code and message.'''
-        pass
-
-    def commit_command(self):
-        ''' Upon both parties signaling success, commit the command, and objects affected '''
-        pass
-
-    def abort_command(self):
-        ''' Upon command failure, undo any modification and free resources '''
-        pass
-
-    def serialize(self):
-        # TODO: representation of the command to send over.
-        pass
-
-    def persist(self):
-        # TODO: define durability model
-        pass
 
 # A model for VASP business environment
 
