f36e588c794c823c3feca011e47a2a700aff311c
Alberto Sonnino <asonnino@fb.com>
2020-03-12 13:28:46 +0000

    Remove typo in networking

--- src/networking.py
+++ src/networking.py
@@ -28,20 +28,3 @@ def process():
     responses = app.vasp.collect_messages()
     responses_json = [command.content for command in responses]
     return jsonify(responses_json)
-
-
-@app.route('/process', methods=['POST'])
-def process():
-    request_json = request.get_json()
-
-    # Recover client's address
-    # This seems ugly -- fix it.
-    diff = json.loads(request_json)['command']['diff']
-    sender_addr = diff['sender']['address']
-    receiver_addr = diff['receiver']['address']
-    other_addr = [sender_addr, receiver_addr][app.vasp.my_addr == sender_addr]
-
-    app.vasp.process_request(LibraAddress(other_addr), request_json)
-    responses = app.vasp.collect_messages()
-    responses_json = [command.content for command in responses]
-    return jsonify(responses_json)
