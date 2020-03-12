8facee4cccc72eaaebfdaf9dbd2fac6d49b37e7e
George Danezis <gdanezis@fb.com>
2020-03-12 13:42:27 +0000

    [CommandProcessor] Moved interface to its own file.

---
+++ src/command_processor.py
@@ -0,0 +1,10 @@
+class CommandProcessor:
+
+    def business_context(self):
+        pass
+
+    def check_command(self, vasp, channel, executor, command, own):
+        pass
+
+    def process_command(self, vasp, channel, executor, command, status, error=None):
+        pass
