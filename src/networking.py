05a21159a78d036dfa7b429c7e0c22d129296fb3
Alberto Sonnino <asonnino@fb.com>
2020-03-16 15:54:49 +0000

    Server network and fix small error in protocol.py

--- src/networking.py
+++ src/networking.py
@@ -1,31 +1,29 @@
-from sample_service import sample_vasp
 from protocol import LibraAddress
 from protocol_messages import CommandResponseObject
 
-from flask import Flask, jsonify, request
-from markupsafe import escape
-import json
-
-app = Flask(__name__)
-app.vasp = sample_vasp(LibraAddress.encode_to_Libra_address(b'B'*16))
+from flask import Flask, request
+from flask.views import MethodView
 
 
-@app.route('/')
-def index():
-    return jsonify(status='OK', message='Hello, world!')
+class Networking:
+    def __init__(self, vasp):
+        self.app = Flask(__name__)
+        self.vasp = vasp
+
+        # Register paths.
+        self.app.add_url_rule(
+            '/'+self.vasp.my_vasp_addr().plain()+'/<other_addr>/process/',
+            view_func=VASPOffChainApi.as_view('vasp_api', self.vasp)
+        )
 
 
-@app.route('/'+app.vasp.my_addr.plain()+'/<other_addr>/process/', methods=['POST'])
-def process(other_addr):
-    other_addr = escape(other_addr).striptags()
-    request_json = request.get_json()
-    app.vasp.process_request(LibraAddress(other_addr), request_json)
-    responses = app.vasp.collect_messages()
+class VASPOffChainApi(MethodView):
+    def __init__(self, vasp):
+        self.vasp = vasp
 
-    responses_json = []
-    for command in responses:
-        if str(command.type) == str(CommandResponseObject):
-            responses_json.append(command.content)
-
-    assert len(responses_json) == 1
-    return responses_json[0]
+    def post(self, other_addr):
+        request_json = request.get_json()
+        channel = self.vasp.get_channel(LibraAddress(other_addr))
+        response = channel.parse_handle_request(request_json)
+        assert str(response.type) == str(CommandResponseObject)
+        return response.content
--- src/test_networking.py
+++ src/test_networking.py
@@ -1,28 +1,31 @@
-from networking import app
-from protocol import LibraAddress
+from networking import *
+from protocol import LibraAddress, OffChainVASP
 from test_sample_service import simple_request_json
 from payment_logic import PaymentCommand
-from protocol_messages import CommandResponseObject
+from executor import CommandProcessor
+from protocol_messages import CommandRequestObject
 
+from unittest.mock import MagicMock
 import pytest
 import json
 
+
 @pytest.fixture
 def client():
-    app.testing = True # Propagate server errors to the client
-    with app.test_client() as c:
+    addr = LibraAddress.encode_to_Libra_address(b'B'*16)
+    processor = MagicMock(spec=CommandProcessor)
+    vasp = OffChainVASP(addr, processor)
+    networking = Networking(vasp)
+    networking.app.testing = True  # Propagate server errors to the client
+    with networking.app.test_client() as c:
         yield c
 
 
-def test_index(client):
-    response = client.get('/')
-    assert response.status_code == 200
-
-
 def test_process(client, simple_request_json):
+    CommandRequestObject.register_command_type(PaymentCommand)
     addr = LibraAddress.encode_to_Libra_address(b'B'*16)
     other_addr = LibraAddress.encode_to_Libra_address(b'A'*16)
     url = '/'+addr.plain()+'/'+other_addr.plain()+'/process/'
-    responses_json = client.post(url, json=simple_request_json)
-    response = json.loads(responses_json.data)
-    assert response['status'] == 'success'
+    response = client.post(url, json=simple_request_json)
+    assert response.status_code == 200
+    assert json.loads(response.data)['status'] == 'success'
