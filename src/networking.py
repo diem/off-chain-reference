c52523199bd4174d0533655630aba9ca8bd5d4ac
Alberto Sonnino <asonnino@fb.com>
2020-03-17 10:15:19 +0000

    Client network

--- src/networking.py
+++ src/networking.py
@@ -1,20 +1,39 @@
 from protocol import LibraAddress
 from protocol_messages import CommandResponseObject
+from business import VASPInfo
 
 from flask import Flask, request
 from flask.views import MethodView
+import requests
+from urllib.parse import urljoin
+import json
 
 
 class Networking:
-    def __init__(self, vasp):
+    def __init__(self, vasp, info_context):
         self.app = Flask(__name__)
         self.vasp = vasp
+        self.info_context = info_context
 
         # Register paths.
         self.app.add_url_rule(
             '/'+self.vasp.my_vasp_addr().plain()+'/<other_addr>/process/',
             view_func=VASPOffChainApi.as_view('vasp_api', self.vasp)
         )
+
+    def run(self):
+        self.app.run()
+
+    def get_url(self, other_addr):
+        base_url = self.info_context.get_base_url()
+        my_addr = self.vasp.my_vasp_addr()
+        url = other_addr.plain() + '/' + my_addr.plain() + '/process/'
+        return urljoin(base_url, url)
+
+    def send_request(self, url, other_addr, json_request):
+        response = requests.post(url, json=json_request)
+        channel = self.vasp.get_channel(other_addr)
+        channel.parse_handle_response(json.dumps(response.json()))
 
 
 class VASPOffChainApi(MethodView):
--- src/test_networking.py
+++ src/test_networking.py
@@ -4,6 +4,7 @@ from test_sample_service import simple_request_json
 from payment_logic import PaymentCommand
 from executor import CommandProcessor
 from protocol_messages import CommandRequestObject
+from business import VASPInfo
 
 from unittest.mock import MagicMock
 import pytest
@@ -11,17 +12,28 @@ import json
 
 
 @pytest.fixture
-def client():
+def network():
     addr = LibraAddress.encode_to_Libra_address(b'B'*16)
     processor = MagicMock(spec=CommandProcessor)
     vasp = OffChainVASP(addr, processor)
-    networking = Networking(vasp)
-    networking.app.testing = True  # Propagate server errors to the client
-    with networking.app.test_client() as c:
+    info_context = MagicMock(spec=VASPInfo)
+    network = Networking(vasp, info_context)
+    return network
+
+
+@pytest.fixture
+def client(network):
+    network.app.testing = True  # Propagate server errors to the client
+    with network.app.test_client() as c:
         yield c
 
 
-def test_process(client, simple_request_json):
+@pytest.fixture
+def simple_response_json():
+    return {"seq": 0, "command_seq": 0, "status": "success"}
+
+
+def test_process_request(client, simple_request_json):
     CommandRequestObject.register_command_type(PaymentCommand)
     addr = LibraAddress.encode_to_Libra_address(b'B'*16)
     other_addr = LibraAddress.encode_to_Libra_address(b'A'*16)
@@ -29,3 +41,11 @@ def test_process(client, simple_request_json):
     response = client.post(url, json=simple_request_json)
     assert response.status_code == 200
     assert json.loads(response.data)['status'] == 'success'
+
+
+# the 'httpserver' fixture comes from the pytest-httpserver package
+def test_send_request(network, httpserver, simple_request_json, simple_response_json):
+    other_addr = LibraAddress.encode_to_Libra_address(b'A'*16)
+    url = '/process'
+    httpserver.expect_request(url).respond_with_json(simple_response_json)
+    network.send_request(httpserver.url_for(url), other_addr, simple_request_json)
