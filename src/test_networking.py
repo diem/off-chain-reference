1f49d0f4568cd7f3e7fe48f6693e875a714227ca
Alberto Sonnino <asonnino@fb.com>
2020-03-23 16:05:04 +0000

    Error handling for networking

--- src/test_networking.py
+++ src/test_networking.py
@@ -1,5 +1,5 @@
 from networking import *
-from protocol import LibraAddress, OffChainVASP
+from protocol import LibraAddress, OffChainVASP, VASPPairChannel
 from test_sample_service import simple_request_json
 from payment_logic import PaymentCommand
 from executor import CommandProcessor
@@ -12,10 +12,19 @@ import json
 
 
 @pytest.fixture
-def network():
-    addr = LibraAddress.encode_to_Libra_address(b'B'*16)
+def tester_addr():
+    return LibraAddress.encode_to_Libra_address(b'A'*16)
+
+
+@pytest.fixture
+def testee_addr():
+    return LibraAddress.encode_to_Libra_address(b'B'*16)
+
+
+@pytest.fixture
+def network(testee_addr):
     processor = MagicMock(spec=CommandProcessor)
-    vasp = OffChainVASP(addr, processor)
+    vasp = OffChainVASP(testee_addr, processor)
     context = MagicMock(spec=VASPInfo)
     network = Networking(vasp, context)
     return network
@@ -29,32 +38,61 @@ def client(network):
 
 
 @pytest.fixture
+def url(tester_addr, testee_addr):
+    return f'/{testee_addr.plain()}/{tester_addr.plain()}/process/'
+
+
+@pytest.fixture
+def bad_request_json():
+    # This requests triggers an exception inside 'json.load' of VASPPairChannel
+    return {"random": "random"}
+
+
+@pytest.fixture
 def simple_response_json():
     return {"seq": 0, "command_seq": 0, "status": "success"}
 
 
-def test_process_request(network, client, simple_request_json):
+# --- Test server ---
+
+
+def test_process_request(network, client, url, simple_request_json):
     CommandRequestObject.register_command_type(PaymentCommand)
-    addr = LibraAddress.encode_to_Libra_address(b'B'*16)
-    other_addr = LibraAddress.encode_to_Libra_address(b'A'*16)
-    url = f'/{addr.plain()}/{other_addr.plain()}/process/'
     network.context.is_authorised_VASP.return_value = True
     response = client.post(url, json=simple_request_json)
     assert response.status_code == 200
     assert json.loads(response.data)['status'] == 'success'
 
 
+def test_process_request_bad_vasp(network, client, url, simple_request_json):
+    network.context.is_authorised_VASP.return_value = False
+    response = client.post(url, json=simple_request_json)
+    assert response.status_code == 401
+
+
+def test_process_request_bad_request(network, client, url, bad_request_json):
+    network.context.is_authorised_VASP.return_value = True
+    response = client.post(url, json=bad_request_json)
+    assert response.status_code == 400
+
+
+# --- Test client ---
+
+
+def test_get_url(network, tester_addr, testee_addr):
+    network.context.get_peer_base_url.return_value = '/'
+    url = network.get_url(tester_addr)
+    assert url == f'/{tester_addr.plain()}/{testee_addr.plain()}/process/'
+
+
 # the 'httpserver' fixture comes from the pytest-httpserver package
-def test_send_request(network, httpserver, simple_request_json, simple_response_json):
-    other_addr = LibraAddress.encode_to_Libra_address(b'A'*16)
+def test_send_request(network, tester_addr, httpserver, simple_request_json,
+                      simple_response_json):
     url = '/process'
     httpserver.expect_request(url).respond_with_json(simple_response_json)
-    network.send_request(httpserver.url_for(url), other_addr, simple_request_json)
+    network.send_request(httpserver.url_for(url), tester_addr, simple_request_json)
 
 
-def test_get_url(network):
-    addr = LibraAddress.encode_to_Libra_address(b'B'*16)
-    other_addr = LibraAddress.encode_to_Libra_address(b'A'*16)
-    network.context.get_peer_base_url.return_value = '/'
-    url = network.get_url(other_addr)
-    assert url == f'/{other_addr.plain()}/{addr.plain()}/process/'
+def test_send_request_unknown_receiver(network, tester_addr, simple_request_json):
+    url = 'http://bad_url'
+    network.send_request(url, tester_addr, simple_request_json)
