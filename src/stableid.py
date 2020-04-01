c7aede7c805c7f9a32c21cb37ef90f3dd818ad5c
George Danezis <gdanezis@fb.com>
2020-04-01 11:43:55 +0100

    [stableid] Added tests and subaddress list

--- src/stableid.py
+++ src/stableid.py
@@ -13,11 +13,12 @@
 
 '''
 
-from util import get_unique_string
+from utils import get_unique_string
 
 ACCOUNT_TAG = 'account'
 STABLEID_TAG = 'stableid'
 SUBADDRESS_TAG = 'subaddress'
+SUBHEAD_TAG = 'subhead'
 
 class SubAddressError(Exception):
     pass
@@ -37,11 +38,21 @@ class SubAddressResolver:
             meta_data = {}
         key = (ACCOUNT_TAG, account_number)
         self.storage[key] = meta_data
+    
+    def get_account_by_number(self, account_number):
+        key = (ACCOUNT_TAG, account_number)
+        if key not in self.storage:
+            raise SubAddressError(f'Account number {account_number} does not exist.')
+        return self.storage[key]
 
 
     def get_stable_id_for_account(self, account_number, period_id):
         ''' Return a stable identifier for an account number per period. 
             Ensure that stable identifiers are not linkable between periods. '''
+        key = (ACCOUNT_TAG, account_number)
+        if key not in self.storage:
+            raise SubAddressError(f'Account number {account_number} does not exist.')
+        
         key = (STABLEID_TAG, account_number, period_id)
         if key not in self.storage:
             self.storage[key] = get_unique_string()
@@ -50,9 +61,24 @@ class SubAddressResolver:
 
     def get_new_subaddress_for_account(self, account_number, scope=None):
         ''' Makes a fresh sub-address with a specific scope '''
+        key = (ACCOUNT_TAG, account_number)
+        if key not in self.storage:
+            raise SubAddressError(f'Account number {account_number} does not exist.')
+
+        # Check if there is a previous subaddress
+        prev_subaddress =  None
+        key_head = (SUBHEAD_TAG, account_number)
+        if key_head in self.storage:
+            prev_subaddress = self.storage[key_head]
+
+        # Create a sub address
         new_subaddress = get_unique_string()
         key = (SUBADDRESS_TAG, new_subaddress) 
-        self.storage[key] = (account_number, scope)
+        self.storage[key] = (account_number, scope, prev_subaddress)
+
+        # Make new subaddress the head of the list
+        self.storage[key_head] = new_subaddress
+
         return new_subaddress
 
     def resolve_subaddress_to_account(self, sub_address):
@@ -60,12 +86,20 @@ class SubAddressResolver:
         key = (SUBADDRESS_TAG, sub_address) 
         if key not in self.storage:
             raise SubAddressError(f'Sub-address {sub_address} does not exist.')
-        account_number, scope = self.storage[key]
+        account_number, scope, _ = self.storage[key]
         return account_number, scope
 
     def get_all_subaddress_by_account(self, account_number):
         # TODO: Make a list structure to keep track of subaccounts
-        raise NotImplementedError()
+        key_head = (SUBHEAD_TAG, account_number) 
+        if key_head not in self.storage:
+            return
+
+        curr_subaddress = self.storage[key_head]
+        while curr_subaddress is not None:
+            yield curr_subaddress
+            key = (SUBADDRESS_TAG, curr_subaddress) 
+            _, _, curr_subaddress = self.storage[key]
 
     # Experimental features
 
---
+++ src/test_stableid.py
@@ -0,0 +1,77 @@
+from stableid import *
+import pytest
+
+@pytest.fixture
+def base():
+    db = {}
+    res = SubAddressResolver(db)
+    return db, res
+
+def test_base_strct(base):
+    db, res = base
+
+    # Create
+    res.register_account_number('ABC')
+    # Get
+    meta = res.get_account_by_number('ABC')
+    assert meta == {}
+
+    # Check existence
+    with pytest.raises(SubAddressError) as _:
+        meta = res.get_account_by_number('ABC_not_exist')
+
+    # Update
+    res.register_account_number('ABC', {'hello':'world'})
+    meta = res.get_account_by_number('ABC')
+    assert meta == {'hello':'world'}
+
+def test_subaddress(base):
+    db, res = base
+
+    # Raises if account does not exist
+    with pytest.raises(SubAddressError) as _:
+        sub = res.get_new_subaddress_for_account('ABC')
+    
+    # Get two new subaddesses, they are unlinkable
+    res.register_account_number('ABC', {'hello':'world'})
+    sub1 = res.get_new_subaddress_for_account('ABC', [ 'scope1' ])
+    sub2 = res.get_new_subaddress_for_account('ABC', [ 'scope2' ])
+    assert sub1 != sub2
+
+    # Resolve a subaddress
+    acc0, scope1 = res.resolve_subaddress_to_account(sub1)
+    assert acc0 == 'ABC'
+    assert scope1 == [ 'scope1' ]
+
+    # Raises if subaddress does not exist
+    with pytest.raises(SubAddressError) as _:
+        acc0, scope1 = res.resolve_subaddress_to_account('NOTEXIST')
+
+def test_stableid(base):
+    db, res = base
+
+    # account does not exist
+    with pytest.raises(SubAddressError) as _:
+        res.get_stable_id_for_account('NOTEXIST', period_id = 10)
+    
+    res.register_account_number('ABC', {'hello':'world'})
+
+    id1 = res.get_stable_id_for_account('ABC', period_id = 10)
+    id1p = res.get_stable_id_for_account('ABC', period_id = 10)
+    id2 = res.get_stable_id_for_account('ABC', period_id = 11)
+
+    assert id1 == id1p
+    assert id1 != id2
+
+def test_iter(base):
+    db, res = base
+    
+    # Make many subaddresses
+    res.register_account_number('ABC', {'hello':'world'})
+    sub1 = res.get_new_subaddress_for_account('ABC', [ 'scope1' ])
+    sub2 = res.get_new_subaddress_for_account('ABC', [ 'scope2' ])
+    sub3 = res.get_new_subaddress_for_account('ABC', [ 'scope3' ])
+
+    # Check they are all returned.
+    L = list(res.get_all_subaddress_by_account('ABC'))
+    assert L == [sub3, sub2, sub1]
