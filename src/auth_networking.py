26c43c4e539dbb5b07e4fb33083d9afa02f9a29d
Alberto Sonnino <alberto.sonnino@ucl.ac.uk>
2020-04-02 12:46:15 +0100

    Update src/auth_networking.py
    Co-Authored-By: Riyaz Faizullabhoy <riyazdf@fb.com>

--- src/auth_networking.py
+++ src/auth_networking.py
@@ -34,7 +34,7 @@ class PeerCertWSGIRequestHandler(werkzeug.serving.WSGIRequestHandler):
     def make_environ(self):
         environ = super(PeerCertWSGIRequestHandler, self).make_environ()
 
-        # Get the cient's certificate.
+        # Get the client's certificate.
         x509_binary = self.connection.getpeercert(True)
         x509 = OpenSSL.crypto.load_certificate(
             OpenSSL.crypto.FILETYPE_ASN1, x509_binary
