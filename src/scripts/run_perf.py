a7f3005b4f3b42d8451c75cf9e5396964906fcda
Alberto Sonnino <asonnino@fb.com>
2020-04-22 16:16:51 +0100

    WIP async perf script

--- src/scripts/run_perf.py
+++ src/scripts/run_perf.py
@@ -1,6 +1,8 @@
 import sys
 sys.path += ['src/.']
+import asyncio
 
-from offchainapi import perf_test
+from offchainapi import perf_test, local_benchmark
 
-perf_test.main_perf()
+#perf_test.main_perf()
+asyncio.run(local_benchmark.main_perf())
