4440021d7ca82a8fca4c295a6c45677e0536255a
Alberto Sonnino <asonnino@fb.com>
2020-04-23 12:49:19 +0100

    Initial aws scripts

---
+++ aws/offchainapi-aws-setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt -y upgrade
+sudo apt -y autoremove
+
+# Install tools to remember GitHub credentials.
+# This is temporary, until we open source the repo. This also mean we have to
+# create a special GitHub API token to upload on the AWS machines and run this
+# script manually until then...
+git config --global credential.helper store
+
+# Get the repo (the testbed brancha).
+git clone -b testbed https://github.com/calibra/off-chain-api.git
+
+# Install Python and all dependencies.
+sudo apt install -y software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install -y python3.7
+python3.7 -m pip install pip
+pip install -r requirements.txt
