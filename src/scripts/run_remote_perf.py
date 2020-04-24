083ad9107958a6c877df4d690f3e03edd9b2732a
Alberto Sonnino <asonnino@fb.com>
2020-04-24 14:24:26 +0100

    Add test client

--- src/scripts/run_remote_perf.py
+++ src/scripts/run_remote_perf.py
@@ -14,12 +14,15 @@ assert len(files) == 2
 other_configs_path = files[1] if my_configs_path == files[0] else files[0]
 
 if num_of_commands > 0:
+    '''
     asyncio.run(
         remote_benchmark.run_client(
             my_configs_path, other_configs_path, num_of_commands
         )
     )
-else:
-    asyncio.run(
-        remote_benchmark.run_server(my_configs_path, other_configs_path)
+    '''
+    remote_benchmark.run_client(
+        my_configs_path, other_configs_path, num_of_commands
     )
+else:
+    remote_benchmark.run_server(my_configs_path, other_configs_path)
