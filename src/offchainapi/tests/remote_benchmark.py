a2f874bd4a1a263083a2b39514bf4cb1b7fc9d15
Alberto Sonnino <asonnino@fb.com>
2020-04-28 11:46:36 +0100

    Minor fix to remote script

--- src/offchainapi/tests/remote_benchmark.py
+++ src/offchainapi/tests/remote_benchmark.py
@@ -165,7 +165,8 @@ def run_client(my_configs_path, other_configs_path, num_of_commands=10, port=Non
 
     async def send_commands(vasp, commands):
         return await asyncio.gather(
-            *[vasp.new_command_async(other_addr, c) for c in commands]
+            *[vasp.new_command_async(other_addr, c) for c in commands],
+            return_exceptions=True
         )
 
     res = asyncio.run_coroutine_threadsafe(send_commands(vasp, commands), loop)
